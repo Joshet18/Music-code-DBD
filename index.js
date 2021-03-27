d57e8cd4336a0fb466b89efb1816354caf2cc6ce
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-26 20:18:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const dbd = require("dbd.js");
+ 
+const bot = new dbd.Bot({
+token: "Tu Token", 
+prefix: "$getServerVar[prefix]"
+})
+ 
+bot.onMessage()
+ 
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
+bot.command({
+name: command.name,
+aliases: commands.aliases,
+code: command.code
+})
+} 
+}
+
+bot.status({
+    text: "Despacito┃$getServerVar[prefix]help $getServerVar[prefix]play",
+    type: "LISTENING",
+    time: 12
+})
+
+bot.status({
+    text: "¡¡$serverCount Servidores!!",
+    type: "WATCHING",
+})
+
+bot.deletedCommand({
+channel: "$channelID",
+code: `$setServerVar[deletedIcon;$userAvatar]
+$setServerVar[deletedAuthor;$username#$discriminator[$authorID]]
+$setServerVar[deletedMessage;$message]`
+})
+bot.onMessageDelete()
+
+bot.variables({
+	prefix: "b+",
+  deletedMessage: "",
+  dedeletedAuthor: "",
+  deletedIcon: ""
+})
+
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+ 
+keepAlive();
+const monitor = new Monitor({
+    website: '',
+    title: 'NAME',
+    interval: 2 // minutes
+});
+ 
+monitor.on('up', (res) => console.log(`${res.website} está encedido.`));
+monitor.on('down', (res) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} se ha parado.`) );
+monitor.on('error', (error) => console.log(error)); 
