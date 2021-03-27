0918ed3349117b9c882c0f5ae9f86c0dd8c14b75
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-26 21:02:26 -0400

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -5,21 +5,32 @@ $description[holi <@$authorID> Esta es mi lista de comandos y mi prefijo es $get
 
 **🎶 Musica**
 **lyrics**
+Obten la letra de la música que esta zonando
 **pause**
+Pausar la música
 **play**
+Poner una musica
 **queue**
+Mostrar la cola de música
 **resume**
-**to**
-**info**
+Reanudar la música
+**skipto (st)**
+Saltar un número de música
+**song-info**
 **stop**
+Detener la música
 **volume**
+Cambiar el volumen de la música
 **skip**
+Salta una música
 **loop**
+Actiavar o desactiva la repetición de la música
 **📩 Información**
 **help**
 Obten la lista de comandos
 **🛠️ Ajutes**
 **set-prefix**
 Cambiar el prefijo del bot
-]`
+]
+$color[08ffef]`
 })
