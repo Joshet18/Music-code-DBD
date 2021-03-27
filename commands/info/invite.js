fe34954dd40205c9deb4c210f518875ced1dbf78
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-27 00:39:11 -0400

    Create invite.js

---
+++ commands/info/invite.js
@@ -0,0 +1,7 @@
+module.exports = ({
+    name: "help",
+    code: `$title[**💌 Invite**]
+$description[aqui tienes mi link para invitarme a tu bonito servidor uwu
+$getBotInvite]
+$color[08ffef]`
+})
