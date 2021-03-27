ea3f16e31507e99f2b3789d07787043e08e41627
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-27 00:10:40 -0400

    Update defaul-prefix.js

--- commands/setups/defaul-prefix.js
+++ commands/setups/defaul-prefix.js
@@ -1,5 +1,6 @@
 module.exports = ({
 name: "defaul-prefix",
 code: `el prefijo se ha cambiado a $getServerVar[defaulPrefix]
-$setServerVar[prefix;$getServerVar[defaulPrefix]]`
+$setServerVar[prefix;$getServerVar[defaulPrefix]]
+$onlyPerms[admin;*No tines permisos para usar este comando*]`
 })
