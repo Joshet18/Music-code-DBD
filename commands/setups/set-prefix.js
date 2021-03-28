58e2fc73c0f56eb8157156faf2692a65c37c2d9b
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-28 03:13:18 -0400

    Update set-prefix.js

--- commands/setups/set-prefix.js
+++ commands/setups/set-prefix.js
@@ -1,6 +1,6 @@
 module.exports = ({
 name: "set-prefix",
-code: `$onlyIf[$message!=]
+code: `$onlyIf[$message!=;Elija un símbolo para su prefijo]
 🛠️ El prefix se ha cambiado a **$message**
 $setServerVar[prefix;$message]
 $onlyPerms[admin;*No tines permisos para usar este comando*]`
