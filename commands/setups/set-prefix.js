760eb7922f04f3d01822d15a98404676336371fe
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-28 03:36:13 -0400

    Delete set-prefix.js

--- commands/setups/set-prefix.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({
-name: "set-prefix",
-code: `$onlyIf[$message!=;Elija un símbolo para su prefijo]
-🛠️ El prefix se ha cambiado a **$message**
-$setServerVar[prefix;$message]
-$onlyPerms[admin;*No tines permisos para usar este comando*]`
-})
