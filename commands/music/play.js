e708b7c3d61dd4b8a2bd2ef21298e8c0075080d1
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-28 19:18:36 -0400

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,5 +1,12 @@
 module.exports = ({
     name: "play",
-    code: `**🎵 Reproduciendo** $playSong[$message;:x: no se puede encontrar la canción]
-$onlyIf[$voiceID!=;Necesitas estar en un canal de voz!]`
+    code: `$description[🎶 Añadido a la cola **$playSong[$message;:x: no se puede encontrar la canción]**]
+    $onlyIf[$message!=;Nombre no valido]
+$onlyIf[$voiceID!=;Necesitas estar en un canal de voz!]
+$thumbnail[$jsonRequest[http://api.somecool.repl.co/yt-search?search=$message;thumbnail;]]
+$color[08c9ff]
+`
 })
+
+
+
