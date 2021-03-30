07551ead09ca170504927b6b7b585d2d19f4ada6
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-30 18:52:32 -0400

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -3,8 +3,8 @@ module.exports = ({
     code: `Volumen: **$message!**
 $onlyIf[$message<=200;El volumen máximo es 200%]
 $onlyIf[$message>=1;El volumen minimo es 1%]
-$onlyIf[$isNumber[$message[1]]==true
-$onlyIf[$voiceID==$voiceID[$clientID]
+$onlyIf[$isNumber[$message[1]]==true]
+$onlyIf[$voiceID==$voiceID[$clientID;Ya estoy reproduciendo música en otro canal]
 $volume[$message]
 `
 })
