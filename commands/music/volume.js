6fac819a00e781a7944d1a05faaeb848d2c4771e
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2021-03-30 18:31:34 -0400

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,6 +1,10 @@
 module.exports = ({
     name: "volume",
     code: `Volumen: **$message!**
+$onlyIf[$message<=200;El volumen máximo es 200%]
+$onlyIf[$message>=1;El volumen minimo es 1%]
+$onlyIf[$isNumber[$message[1]]==true
+$onlyIf[$voiceID==$voiceID[$clientID]
 $volume[$message]
-$isNumber[$message]`
+`
 })
